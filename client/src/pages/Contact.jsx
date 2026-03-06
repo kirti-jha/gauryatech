ab465c2d4a527a89992cbfd77aa4a737a9360dcd
kirti jha <kjha0141@gmail.com>
2026-03-06 12:36:25 +0530

    Remove Reach Out banner from Contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ContactHero from '../components/contact/ContactHero';
 import ContactFormMap from '../components/contact/ContactFormMap';
-import ContactBanner from '../components/contact/ContactBanner';
 import ServicesCTA from '../components/services/ServicesCTA';
 import Footer from '../components/Footer';
 
@@ -10,7 +9,6 @@ const Contact = () => {
     <div className="min-h-screen pt-20">
       <ContactHero />
       <ContactFormMap />
-      <ContactBanner />
       <ServicesCTA />
       <Footer />
     </div>
